c117966351b273cc43e7edfbb15af7218c52ced2
Karkola-D <dylan.karkola@web.de>
2023-09-06 14:29:03 +0200

    New Headline

---
+++ Das Martingale-Spiel.py
@@ -0,0 +1,39 @@
+import random
+
+def martingale(initial_bet, rounds):
+  """
+  Simuliert das Martingale-Spiel für die angegebenen Runden.
+  Simulates the Martingale-game for the given rounds 
+
+  Args:
+    initial_bet: Die Anfangsbeting
+    rounds: Die Anzahl der Runden
+    
+    initial_bet: The initial bet rounds: The number of rounds
+
+  Returns:
+    Eine Liste der Gewinne oder Verluste
+    A list of wins and losses 
+  """
+  wins = []
+  losses = []
+  bet = initial_bet
+  for _ in range(rounds):
+    win = random.choice([True, False])
+    if win:
+      wins.append(bet)
+      bet = initial_bet
+    else:
+      bet *= 2
+      losses.append(bet)
+  return wins, losses
+
+if __name__ == "__main__":
+  wins, losses = martingale(10, 10)
+  print(f"Gewinne: {wins}")
+  print(f"Verluste: {losses}")
+  print(f"Anzahl der Wins: {len(wins)}")
+  print(f"Anzahl der Verluste: {len(losses)}")
+  
+  
+  
