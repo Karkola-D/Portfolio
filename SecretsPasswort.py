f8a0698316322cc1c12e54397a9cd9d4c8a6d965
Karkola-D <dylan.karkola@web.de>
2023-11-09 18:25:39 +0100

    New Headline

--- SecretsPasswort.py
+++ SecretsPasswort.py
@@ -1,9 +1,9 @@
 import secrets
 import string
 
-"""print(secrets.choice("ab"))"""
+#print(secrets.choice("ab"))
 
 chars = string.digits + string.ascii_letters + string.punctuation
 print(len(chars)) # Entropie: 6,555 , Min: 100Bit
-print(100/6.555) # 16
+print(100/6.555) # 16 
 print("".join(secrets.choice(chars) for _ in range(40)))
