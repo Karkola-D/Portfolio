6cd737be5c5c593ea57d268e23286e6835c6d493
Karkola-D <dylan.karkola@web.de>
2023-11-29 00:41:04 +0100

    new update

--- SecretsPasswort.py
+++ SecretsPasswort.py
@@ -5,5 +5,5 @@ import string
 
 chars = string.digits + string.ascii_letters + string.punctuation
 print(len(chars)) # Gibt die länge des chars an. "94 Buchstaben enthalten"                   Entropie: 6,555 , Min: 100Bit vorgabe vom BND.
-print(100/6.555) # print(100/6.555) = 16 // Advanced Encryption Standard(AES) print(256/6.555) = 40   6.555 Entropie ist im jeden Buchstaben 
+print(100/6.555) # print(100/6.555) = 16 // Advanced Encryption Standard(AES) print(256/6.555) = 40   6.555 Entropie ist im jeden Buchstaben enthalten
 print("Deine neues Passwort:","".join(secrets.choice(chars) for _ in range(40)))
