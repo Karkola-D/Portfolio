a8f4fcc8de540065da036d6611ab748380714f54
Karkola-D <dylan.karkola@web.de>
2023-11-28 19:37:40 +0100

    new update

--- SecretsPasswort.py
+++ SecretsPasswort.py
@@ -4,6 +4,6 @@ import string
 #print(secrets.choice("ab"))
 
 chars = string.digits + string.ascii_letters + string.punctuation
-print(len(chars)) # Entropie: 6,555 , Min: 100Bit
+print(len(chars)) # Entropie: 6,555 , Min: 100Bit. Gibt die länge des chars an.
 print(100/6.555) # 16 
-print("".join(secrets.choice(chars) for _ in range(40)))
+print("Deine neues Passwort:","".join(secrets.choice(chars) for _ in range(40)))
