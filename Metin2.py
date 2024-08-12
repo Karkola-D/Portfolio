67f2283a87eaa366c42f02399d884e9dcccd1a7c
Karkola-D <142596294+Karkola-D@users.noreply.github.com>
2024-08-13 00:11:36 +0200

    Update Metin2.py

--- Metin2.py
+++ Metin2.py
@@ -1,1 +1,19 @@
 
+from collections import Counter
+
+Overwatch_Spieler_Ingame_Item = {
+"yang": 100000,
+"DIVA": 16
+
+}
+
+Ingame_Shop = {
+    "yang": 1000,
+    "DIVA": 1
+}
+
+neue_Ingame = Counter(Overwatch_Spieler_Ingame_Item)
+neue_Ingame.subtract(Ingame_Shop)
+print(neue_Ingame)
+for item, anzahl in neue_Ingame.items():
+    print(f'{item}:{anzahl}:')
